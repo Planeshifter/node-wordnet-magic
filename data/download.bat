d19d277d5cef10bcf58816caaf4f86fd28de6a08
Philipp Burckhardt <pburckhardt@outlook.com>
2014-09-03 00:20:32 -0400

    download tools windows + unix

---
+++ data/download.bat
@@ -0,0 +1,3 @@
+curl -L "http://downloads.sourceforge.net/project/wnsql/wnsql3/sqlite/3.1/sqlite-31.db.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwnsql%2Ffiles%2Fwnsql3%2Fsqlite%2F3.1%2Fsqlite-31.db.zip%2Fdownload&ts=1409711250&use_mirror=iweb" -o sqlite-31.zip
+unzip sqlite-31.zip
+DEL sqlite-31.zip
--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget 'http://downloads.sourceforge.net/project/wnsql/wnsql3/sqlite/3.1/sqlite-31.db.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fwnsql%2Ffiles%2Fwnsql3%2Fsqlite%2F3.1%2Fsqlite-31.db.zip%2Fdownload&ts=1409711250&use_mirror=iweb' -O "sqlite-31.zip"
-unzip sqlite-31.zip -d data
+unzip sqlite-31.zip 
 rm sqlite-31.zip
